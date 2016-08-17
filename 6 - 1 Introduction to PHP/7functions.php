6bc62780c1213359f4121da92bc4364575d1a74f
Johnnie2016 <j.morrison12@rgu.ac.uk>
2016-08-17 21:45:16 +0100

    Introduction to PHP files added

---
+++ 6 - 1 Introduction to PHP/7functions.php
@@ -0,0 +1,27 @@
+<?php
+
+//Create the array
+$lotteryArray = array("Mike", "John", "Mark", "Angela", "Roger", "Chrisina", "Suzanne", "Sue" , "Ines", "Ian");
+
+//Sort the array
+sort($lotteryArray);
+
+//Select the winner
+$winner = (rand(0,count($lotteryArray))) - 1;
+
+//Prints the winner
+echo "<p>The winner of all the specs is " . strtoupper($lotteryArray[$winner]) . "</p>";
+
+//Remove the person from the array
+unset($array[$winner]);
+
+//Do the same again for mugs
+$winner = (rand(0,count($lotteryArray))) - 1;
+echo "<p>The winner of all the mugs is " . strtoupper($lotteryArray[$winner]) . "</p>";
+unset($array[$winner]);
+
+//and the sausuage rolls
+$winner = (rand(0,count($lotteryArray))) - 1;
+echo "<p>The winner of all the sausage rolls is " . strtoupper($lotteryArray[$winner]) . "</p>";
+
+?>
