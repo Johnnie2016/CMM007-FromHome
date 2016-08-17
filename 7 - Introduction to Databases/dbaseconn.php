38f123b40935c0eb4e7d6bf923c71cda797755de
Johnnie2016 <j.morrison12@rgu.ac.uk>
2016-08-17 19:20:14 +0100

    Introduction to Dbases files added

---
+++ 7 - Introduction to Databases/dbaseconn.php
@@ -0,0 +1,8 @@
+<?php
+    define('DB_SERVER', 'br-cdbr-azure-south-a.cloudapp.net');
+    define('DB_USERNAME', 'b3ac9c625849d8');
+    define('DB_PASSWORD', 'f04f3dfe');
+    define('DB_DATABASE', 'JM0207753');
+
+    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
