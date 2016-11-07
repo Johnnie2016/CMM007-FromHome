2052448da64b05dd1b9c2d91458b96b0414fa7b6
Johnnie <j.morrison12@rgu.ac.uk>
2016-11-07 16:02:04 +0000

    Get Method

---
+++ 8 - Get Post and Forms/form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Form to input details</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<form action="formResponse.php" method="post">
+    <label>Surname </label><input type="text" name="surname"><br><br>
+    <label>Name </label><input type="text" name="name"><br><br>
+    <label>Gender </label><input type="text" name="gender"><br><br>
+    <label>Main Superhero Power </label><input type="text" name="power">
+    <input type="submit" value="Submit">
+</form>
+
+
+</body>
+</html>
+
+
+
