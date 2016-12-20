8a172b70f0e927affcc8ff958194868df11374b5
Johnnie <j.morrison12@rgu.ac.uk>
2016-12-20 22:29:37 +0000

    Edit XML gen file

--- Google Earth PHP KML/genxml.php
+++ Google Earth PHP KML/genxml.php
@@ -2,8 +2,7 @@
 $username = 'b3ac9c625849d8';
 $password = 'f04f3dfe';
 $database = 'jm0207753';
-$server = 'br-cdbr-azure-south-a.cloudapp.net';
- 
+$server = 'br-cdbr-azure-south-a.cloudapp.net'; 
 error_reporting(0);
 function parseLocation(){
 $radius = $_GET['radius'];
