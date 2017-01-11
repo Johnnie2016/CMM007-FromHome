25017b42a6d2dca3fd8e2018bbccd4195e699ed7
Johnnie <j.morrison12@rgu.ac.uk>
2017-01-11 23:58:13 +0000

    small change

--- Google Earth PHP KML/dbconcomplex.php
+++ Google Earth PHP KML/dbconcomplex.php
@@ -4,7 +4,7 @@ define('DB_USERNAME', 'b3ac9c625849d8');
 define('DB_PASSWORD', 'f04f3dfe');
 define('DB_DATABASE', 'jm0207753');
 
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 
 if (!$db) {
