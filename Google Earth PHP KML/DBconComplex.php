c6d81a073c00e1c0bf2dfbbd8f73ce3ba615fbfb
Johnnie <j.morrison12@rgu.ac.uk>
2017-01-11 23:38:44 +0000

    small change

--- Google Earth PHP KML/DBconComplex.php
+++ Google Earth PHP KML/DBconComplex.php
@@ -3,6 +3,7 @@ define('DB_SERVER', 'br-cdbr-azure-south-a.cloudapp.net');
 define('DB_USERNAME', 'b3ac9c625849d8');
 define('DB_PASSWORD', 'f04f3dfe');
 define('DB_DATABASE', 'jm0207753');
+
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 // test if connection was established, and print any errors
