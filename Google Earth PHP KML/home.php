adf61aaea0373a06ab831279db505d3411ecbbf9
Johnnie <j.morrison12@rgu.ac.uk>
2017-01-19 21:44:19 +0000

    Link to local file added

--- Google Earth PHP KML/home.php
+++ Google Earth PHP KML/home.php
@@ -12,7 +12,7 @@
 Click here to go straight to the North Sea UKCS as seen in Google Earth.<p>
 N.B. - In order to use this you will need to have Google Earth installed.
 
-<p><a href="C:\Users\Johnnie\Desktop\Studies\Robert Gordon University\RGU 2015\Semester 3 - Feb 16 to June 16\MSc Dissertation\MSc Investigation research\KML basic structure\Working copies\Chapter 6\BasicNetworkLinkPointsatwebserver.kml?">Open Google Earth...</a></p>
+<p><a href="file///C:\Users\Johnnie\Desktop\Studies\Robert Gordon University\RGU 2015\Semester 3 - Feb 16 to June 16\MSc Dissertation\MSc Investigation research\KML basic structure\Working copies\Chapter 6\BasicNetworkLinkPointsatwebserver.kml?">Open Google Earth...</a></p>
 
 <h3>Or would you like to search manually below ?</h3><p>
 
