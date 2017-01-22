ad8117ff2484f15b045f5a9a16cf77c3e74ace5c
Johnnie <j.morrison12@rgu.ac.uk>
2017-01-22 14:25:52 +0000

    Added new display page

--- Google Earth PHP KML/home.php
+++ Google Earth PHP KML/home.php
@@ -18,31 +18,20 @@ N.B. - In order to use this you will need to have Google Earth installed.
 <h3>Or would you like to search manually below ?</h3><p>
 </header>
 <main>
-    <form action="displayWellreg.php" method="post">
-        <p>Select the Well registration of the data you are interested in</p>
-        <select name="wellregistration">
-            <?php
-            include ("dbconcomplex.php");
-            $sql_query = "SELECT RowID, OrigID, WellID, WellRegistration, PercSand, Hydrocarbon, Cored FROM coredUKHCorNotes";
-            $result = $db->query($sql_query);
-            while($row = $result->fetch_array()) {
-                $OrigID = $row['OrigID'];
-                $WellID = $row['WellID'];
-                $WellRegistration = $row['WellRegistration'];
-                $id = $row['RowID'];
-                echo "<option value='{$id}'>{$WellRegistration}</option>";
-            }
-            ?>
-        </select><br><p>
-        <input type="submit" text="Display Well details">
-    </form><p>
+
+    <!--Paste SEARCH BOX 1 BACK IN HERE. -----  SEARCH BOX 1 BACK IN HERE.-->
+
+
+
+
+    <p>
 
         <form action="displayWellregTestDB.php" method="post">
-            <p>Select the Well registration of the data you are interested in</p>
+            <p>Select the Well registration of the test data you are interested in</p>
             <select name="wellregistration">
                 <?php
                 include ("dbconcomplex.php");
-                $sql_query = "SELECT ID, WellID, WellRegistration, PercSand, Hydrocarbon, Cored FROM coredukwells";
+                $sql_query = "SELECT WellID, WellRegistration, PercSand, Hydrocarbon, Cored FROM coredukwells";
                 $result = $db->query($sql_query);
                 while($row = $result->fetch_array()) {
                     $WellID = $row['WellID'];
