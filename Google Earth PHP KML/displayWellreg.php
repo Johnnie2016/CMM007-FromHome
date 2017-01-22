e4ace254f8e40244007210adc62e8ddad1d70943
Johnnie <j.morrison12@rgu.ac.uk>
2017-01-22 11:30:39 +0000

    Added new display page

---
+++ Google Earth PHP KML/displayWellreg.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Well Registration selection</title>
+</head>
+<body>
+<header>
+    <h1>Data4WellCore</h1>
+    <h2>Display Wells select via Well registration number</h2>
+    <p><a href="home.php">Return to home...</a></p>
+</header>
+<main>
+    <?php
+
+         include("dbconcomplex.php");
+
+         if(isset($_GET['wellregistration'])) {
+         $WellRegistration = $_GET['wellregistration'];
+         $sql_query = "SELECT WellRegistration, WellID, PercSand, Hydrocarbon, Cored FROM coredUKHCorNotes where WellRegistration = '$WellRegistration'";
+        }
+        else {
+            $sql_query = "SELECT * FROM coredUKHCorNotes";
+        }
+
+        $result = $db->query($sql_query);
+      while($row = $result->fetch_array())
+      {
+       $OrigID = $row['OrigID'];
+       $WellID = $row['WellID'];
+       $WellRegistration = $row['WellRegistration'];
+       $PercSand = $row['PercSand'];
+       $Hydrocarbon = $row['Hydrocarbon'];
+       $Cored = $row['Cored'];
+       echo "<article>
+             <p> The Well with Well Registration<strong>{$WellRegistration}</strong> has the WellID <strong>{$WellID}</strong>and contains the following percentage of sand<strong>{$PercSand}</strong> and was cored (Y or N) <strong>{$Cored}</strong> </p>";
+      }
+    ?>
+
+</main>
+</body>
+</html>
