d3931a6c15c5c0de6cd142ff009f44fa9bcd6f33
Johnnie <j.morrison12@rgu.ac.uk>
2017-01-22 02:18:26 +0000

    small amendments

--- 7 - Marvel Films/index.php
+++ 7 - Marvel Films/index.php
@@ -7,10 +7,10 @@
 </head>
 
 <body>
-    <a href="allfilms.php">Display all films</a>
-    <a href="marvelfilms.php">Display films created by Marvel Studios</a>
-    <a href="post2010films.php">Display films created after 2010</a>
-    <a href="xmenfilms.php">Display X-Men films</a>
+    <a href="allfilms.php">Display all films</a><br>
+    <a href="marvelfilms.php">Display films created by Marvel Studios</a><br>
+    <a href="post2010films.php">Display films created after 2010</a><br>
+    <a href="xmenfilms.php">Display X-Men films</a><br>
 </body>
 </html>
 
