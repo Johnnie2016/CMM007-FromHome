5931c7e4aa76c30a56652e2ac23a85697a1f49e6
Johnnie <j.morrison12@rgu.ac.uk>
2017-01-22 17:20:49 +0000

    Added new display page

--- Google Earth PHP KML/home.php
+++ Google Earth PHP KML/home.php
@@ -19,9 +19,30 @@ N.B. - In order to use this you will need to have Google Earth installed.
 </header>
 <main>
 
-    <!--Paste SEARCH BOX 1 BACK IN HERE. -----  SEARCH BOX 1 BACK IN HERE.-->
-
-
+    <form action="displayCoreDealDB.php" method="post">
+        <p>Select the Well registration of the core data you are interested in</p>
+        <select name="wellregistration">
+            <?php
+            include ("dbconcomplex.php");
+            $sql_query = "SELECT * FROM dealexportapr";
+            $result = $db->query($sql_query);
+            while($row = $result->fetch_array()) {
+                $RowID = $row['RowID'];
+                $WellRegistration = $row['Well_Reg_No'];
+                $SampleType = $row['Sample_Type_Deal'];
+                $Feet = $row['Ft'];
+                $TopDepth = $row['Top_Depth'];
+                $BottomDepth = $row['Bottom_Depth'];
+                $Preservation = $row['Preservation'];
+                $CoreNo = $row['Core_No'];
+                $id = $row['RowID'];
+                echo "<option value='{$id}'>{$WellRegistration}</option>";
+            }
+            ?>
+        </select><br><p>
+            <input type="submit" text="Display Core details">
+    </form>
+    <!--Another form could be here-->
     <p>
 
         <form action="displayWellregTestDB.php" method="post">
