4fc271d32d52bb27ebf4f7ddffcedf3d543d2c12
Johnnie <j.morrison12@rgu.ac.uk>
2017-01-22 14:01:47 +0000

    Added new display page

--- Google Earth PHP KML/home.php
+++ Google Earth PHP KML/home.php
@@ -21,7 +21,7 @@ N.B. - In order to use this you will need to have Google Earth installed.
     <form action="displayWellreg.php" method="post">
         <p>Select the Well registration of the data you are interested in</p>
         <select name="wellregistration">
-            <?
+            <?php
             include ("dbconcomplex.php");
             $sql_query = "SELECT OrigID, RowID, WellRegistration, WellID, PercSand, Hydrocarbon, Cored FROM coredUKHCorNotes";
             $result = $db->query($sql_query);
@@ -35,7 +35,27 @@ N.B. - In order to use this you will need to have Google Earth installed.
             ?>
         </select><br><p>
         <input type="submit" text="Display Well details">
-     </form>
+    </form><p>
+
+        <form action="displayWellregTestDB.php" method="post">
+            <p>Select the Well registration of the data you are interested in</p>
+            <select name="wellregistration">
+                <?php
+                include ("dbconcomplex.php");
+                $sql_query = "SELECT OrigID, RowID, WellRegistration, WellID, PercSand, Hydrocarbon, Cored FROM coredukwells";
+                $result = $db->query($sql_query);
+                while($row = $result->fetch_array()) {
+                    $OrigID = $row['OrigID'];
+                    $WellID = $row['WellID'];
+                    $WellRegistration = $row['WellRegistration'];
+                    $id = $row['RowID'];
+                    echo "<option value='{$id}'>{$WellRegistration}</option>";
+                }
+                ?>
+            </select><br><p>
+            <input type="submit" text="Display Well details">
+        </form>
+
 </main>
 
 </body>
