6a329db4d5c492f6162e4c9777c5d8d8386eed16
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-04 15:14:17 +0100

    Styling

--- Google Earth PHP KML/Further Development/index.php
+++ Google Earth PHP KML/Further Development/index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="utf-8">
     <title>Login with your username and password</title>
-    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+    <link rel="stylesheet" href="assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/CSS/unsemantic-grid-responsive-tablet.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' type='text/css'>
 </head>
+
 <body>
+<header>
+    <img src="assets/images/gusherderrick.png" alt="Struck oil" id="gusherderrick">
+    <img src="assets/images/Header%20logo%20Orange.PNG" alt="header" id="headerlogo">
+</header>
+
+
+
+
+
 <h1>Please login with your username and password</h1>
 
 <p><a href="newuserform.html">If you do not have an account, please register for one here...</a></p>
