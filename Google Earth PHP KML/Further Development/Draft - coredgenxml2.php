83b30ffa62015a21c58d11a6444e0f5caf1f9e56
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-06 14:49:20 +0100

    Display on GE

--- Google Earth PHP KML/Further Development/Draft - coredgenxml2.php
+++ Google Earth PHP KML/Further Development/Draft - coredgenxml2.php
@@ -60,10 +60,7 @@ $node = $dom->createElement("Placemark");
 $placenode = $parnode->appendChild($node);$namenode = $dom->createElement("name",htmlentities ($row['WellID']));
 $placenode->appendChild($namenode);
 $descriptioncdata = $dom->createCDATASection("<b>Well Registration:</b> " .
-$row['WellRegistration'] ."<br/><b>Distance:</b> " . $row['distance']
-        . !$name
-        <p>
-    . !$description);
+$row['WellRegistration'] ."<br/><b>Distance:</b> " . $row['distance']);
 $descriptionnode=$dom->createElement("description");
 $descriptionnode->appendChild($descriptioncdata);
 $placenode->appendChild($descriptionnode);
