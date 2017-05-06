12d576a16bff098ca200ccdfc791736c27e2dbf5
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-06 22:51:48 +0100

    User selection amended

--- Google Earth PHP KML/Further Development/displayCoreDealDB.php
+++ Google Earth PHP KML/Further Development/displayCoreDealDB.php
@@ -23,8 +23,8 @@
          include("dbconcomplex.php");
             if(isset($_POST['wellregistration'])) {
             //$WellRegistration = $_GET['wellregistration'];
-            //echo $_POST['wellregistration'];
-            //{
+            echo $_POST['wellregistration'];
+
             $WellRegistration = $_POST['wellregistration'];
             $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr where Well_Reg_No = '$WellRegistration'";
             }
