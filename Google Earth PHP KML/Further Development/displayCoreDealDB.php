b5277000efe95cbf888f635eda3226d79fd9ef6b
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-12 23:32:41 +0100

    Testing dropdown

--- Google Earth PHP KML/Further Development/displayCoreDealDB.php
+++ Google Earth PHP KML/Further Development/displayCoreDealDB.php
@@ -21,16 +21,16 @@
 <main>
     <?
          include("dbconcomplex.php");
-            //if(isset($_POST['wellregistration'])) {
+            if(isset($_POST['wellregistration'])) {
 
-            //{
+            
             $WellRegistration = $_POST["wellregistration"];
             echo $WellRegistration;
             $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr where Well_Reg_No = '$WellRegistration'";
-            //}
-            else //{
+            }
+            else {
                 $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr";
-            //}
+            }
 
              $result = $db->query($sql_query);
               while($row = $result->fetch_array())
