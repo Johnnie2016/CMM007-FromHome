616443dd0bf0494b7eb5e4fe39d1235233c6a721
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-12 23:17:26 +0100

    Testing new dropdown

--- Google Earth PHP KML/Testing only/displayCoreDealDB.php
+++ Google Earth PHP KML/Testing only/displayCoreDealDB.php
@@ -16,7 +16,7 @@
 
          if(isset($_POST["wellregistration"])) {
 
-          //$wellregistration = $_POST["wellregistration"];
+          $wellregistration = $_POST["wellregistration"];
           //echo $wellregistration;
 
          $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr WHERE Well_Reg_No = '$wellregistration'";
