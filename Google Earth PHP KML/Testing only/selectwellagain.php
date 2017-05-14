0b477978ef9ec8d7ccba5183a73ec5d2a19a2708
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-14 19:36:58 +0100

    Dropdown box selection

--- Google Earth PHP KML/Testing only/selectwellagain.php
+++ Google Earth PHP KML/Testing only/selectwellagain.php
@@ -5,14 +5,14 @@ include("dbconcomplex.php");
 //$firstname = $_POST["firstname"];
 //$lastname = $_POST["lastname"];
 //$superheroname = $_POST["superheroName"];
-$wellregistration = $_POST["wellregistration"];
+$WellReg = $_POST["wellregistration"];
 
 // $sql = "INSERT INTO superheroes (firstname, lastname, superheroName, mainSuperpower) VALUES ('$firstname', '$lastname', '$superheroname', '$mainsuperpower')";
-$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr WHERE Well_Reg_No = '$wellregistration'";
+$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr WHERE Well_Reg_No = '$WellReg'";
 
   if (mysqli_query($db, $sql_query)) {
     echo "<article>
-             <p> Well Registration <strong> {$wellregistration} </strong> has samples of type <strong> {$SampleType}</strong> in storage which has a top depth of <strong>{$TopDepth}</strong><strong>{$Feet}</strong>
+             <p> Well Registration <strong> {$WellReg} </strong> has samples of type <strong> {$SampleType}</strong> in storage which has a top depth of <strong>{$TopDepth}</strong><strong>{$Feet}</strong>
               <p>and a bottom depth of <strong>{$BottomDepth}</strong><strong>{$Feet}</strong>. The core number is <strong>{$CoreNo}</strong> and the type of preservation is <strong> {$Preservation} </strong></p></article>";
     }
 
