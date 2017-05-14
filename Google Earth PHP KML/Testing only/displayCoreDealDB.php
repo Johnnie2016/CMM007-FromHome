e5967c3af6d786c371ace4484fb401ab3c35645e
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-14 23:34:38 +0100

    Dropdown box

--- Google Earth PHP KML/Testing only/displayCoreDealDB.php
+++ Google Earth PHP KML/Testing only/displayCoreDealDB.php
@@ -8,34 +8,33 @@
 <header>
     <h1>Data4WellCore</h1>
     <h2>Display Core details via Well registration number</h2>
-    <p><a href="superheroform.php">Return to search page...</a></p>
+    <p><a href="selectwellform.php">Return to search page...</a></p>
 </header>
 <main>
     <?
          include("dbconcomplex.php");
+         $wellregistration = $_POST["wellregistration"];
+         //echo $wellregistration;
 
          if(isset($_POST["wellregistration"])) {
 
-          $wellregistration = $_POST["wellregistration"];
-          //echo $wellregistration;
+             $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr WHERE Well_Reg_No = '$wellregistration'";
+             }
+             else {
+             $sql_query = "SELECT * FROM dealexportapr";
+             }
 
-         $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr WHERE Well_Reg_No = '$wellregistration'";
-        }
-        else {
-        $sql_query = "SELECT * FROM dealexportapr";
-        }
-
-        $result = $db->query($sql_query);
-      while($row = $result->fetch_array())
-      {
-       $ID = $row['RowID'];
-       $wellregistration = $row['Well_Reg_No'];
-       $SampleType = $row['Sample_Type_Deal'];
-       $Feet = $row['Ft'];
-       $TopDepth = $row['Top_Depth'];
-       $BottomDepth = $row['Bottom_Depth'];
-       $CoreNo = $row['Core_No'];
-          $Preservation = $row['Preservation'];
+             $result = $db->query($sql_query);
+             while($row = $result->fetch_array())
+         {
+             $ID = $row['RowID'];
+             $wellregistration = $row['Well_Reg_No'];
+             $SampleType = $row['Sample_Type_Deal'];
+             $Feet = $row['Ft'];
+             $TopDepth = $row['Top_Depth'];
+             $BottomDepth = $row['Bottom_Depth'];
+             $CoreNo = $row['Core_No'];
+             $Preservation = $row['Preservation'];
 
        echo "<article>
              <p> Well Registration <strong> {$wellregistration} </strong> has samples of type <strong> {$SampleType}</strong> in storage which has a top depth of <strong>{$TopDepth}</strong><strong>{$Feet}</strong>
