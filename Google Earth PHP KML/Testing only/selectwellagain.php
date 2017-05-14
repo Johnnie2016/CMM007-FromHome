eb18f3c52ea3b5cf2811ee502fd6fc67fb40c567
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-14 19:30:46 +0100

    Dropdown box test

--- Google Earth PHP KML/Testing only/selectwellagain.php
+++ Google Earth PHP KML/Testing only/selectwellagain.php
@@ -5,17 +5,21 @@ include("dbconcomplex.php");
 //$firstname = $_POST["firstname"];
 //$lastname = $_POST["lastname"];
 //$superheroname = $_POST["superheroName"];
-$wellreg = $_POST["wellregistration"];
+$wellregistration = $_POST["wellregistration"];
 
 // $sql = "INSERT INTO superheroes (firstname, lastname, superheroName, mainSuperpower) VALUES ('$firstname', '$lastname', '$superheroname', '$mainsuperpower')";
-$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr WHERE Well_Reg_No = '$wellreg'";
+$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr WHERE Well_Reg_No = '$wellregistration'";
 
-if (mysqli_query($db, $sql_query)) {
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($db);
-}
+  if (mysqli_query($db, $sql_query)) {
+    echo "<article>
+             <p> Well Registration <strong> {$wellregistration} </strong> has samples of type <strong> {$SampleType}</strong> in storage which has a top depth of <strong>{$TopDepth}</strong><strong>{$Feet}</strong>
+              <p>and a bottom depth of <strong>{$BottomDepth}</strong><strong>{$Feet}</strong>. The core number is <strong>{$CoreNo}</strong> and the type of preservation is <strong> {$Preservation} </strong></p></article>";
+    }
 
-header("location:index.php");
+    else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($db);
+    }
+
+    header("location:index.php");
 ?>
 
-<!-- Why is this not working -->
--- Google Earth PHP KML/Testing only/selectwellformagain.php
+++ Google Earth PHP KML/Testing only/selectwellformagain.php
@@ -44,4 +44,12 @@
 </body>
 </html>
 
-<!-- Why is this not working -->
+<!--
+<form action="formResponse.php" method="post">
+    <label>Surname </label><input type="text" name="surname"><br><br>
+    <label>Name </label><input type="text" name="name"><br><br>
+    <label>Gender </label><input type="text" name="gender"><br><br>
+    <label>Main Superhero Power </label><input type="text" name="power">
+    <input type="submit" value="Submit">
+</form>
+-->
