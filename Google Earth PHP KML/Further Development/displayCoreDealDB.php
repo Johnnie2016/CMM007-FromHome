cc4311b882aced959eef8ab5a85d6eae50fe7f4d
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-15 17:12:06 +0100

    Dropdown box

--- Google Earth PHP KML/Further Development/displayCoreDealDB.php
+++ Google Earth PHP KML/Further Development/displayCoreDealDB.php
@@ -35,7 +35,7 @@
             $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr where Well_Reg_No = $WellRegistration";
 
             //else
-                $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr";
+                //$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr";
 
 
              $result = $db->query($sql_query);
