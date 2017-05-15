e4165bfc005961c95c46d432cab7a2038be9fbdc
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-16 00:54:05 +0100

    Folder name change

--- Google Earth PHP KML/Further Development/displayCoreDealDB.php
+++ Google Earth PHP KML/Further Development/displayCoreDealDB.php
@@ -22,20 +22,18 @@
     <?
          include("dbconcomplex.php");
 
-
-
             $WellReg = $_POST["wellregistration"];
             echo $WellReg;
-            //$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr where Well_Reg_No = $WellReg";
-    $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr where Well_Reg_No = '" . $WellReg ."'";
-    //$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr where Well_Reg_No = '#10/01 -A 10'";
+
+            $sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr where Well_Reg_No = '" . $WellReg ."'";
+            //$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr where Well_Reg_No = '#10/01 -A 10'";
             //$sql_query = "SELECT Well_Reg_No, Ft FROM dealexportapr where Well_Reg_No = $WellReg";
             //$sql_query = "SELECT Well_Reg_No, Ft FROM dealexportapr where Well_Reg_No = '#10/01 -A 10'";
             //else
                 //$sql_query = "SELECT RowID, Well_Reg_No, Sample_Type_Deal, Ft, Top_Depth, Bottom_Depth, Preservation, Core_No FROM dealexportapr";
 
 
-             $result = $db->query($sql_query);
+            $result = $db->query($sql_query);
              echo $WellReg;
               while($row = $result->fetch_array())
             {
