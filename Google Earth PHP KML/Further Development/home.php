f99bc914c7d3ee90100644d7be458e8244a50d3e
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-15 16:33:56 +0100

    Folder name change

--- Google Earth PHP KML/Further Development/home.php
+++ Google Earth PHP KML/Further Development/home.php
@@ -23,7 +23,7 @@
 
 
 <main>
-
+    
     <form action="displayCoreDealDB.php" method="post">
         <h3>Select the Well registration of the core data you are interested in</h3>
         <select name="wellregistration">
@@ -41,7 +41,7 @@
                 //$Preservation = $row["Preservation"];
                 //$CoreNo = $row["Core_No"];
                 $id = $row["RowID"];
-                echo "<option value='{$id}'>{$WellRegistration}</option>";
+                echo "<option value='{$WellRegistration}'>{$WellRegistration}</option>";
             }
             ?>
         </select><br><br>
