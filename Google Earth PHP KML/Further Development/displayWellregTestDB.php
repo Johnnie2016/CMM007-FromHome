db412adebfd21ebf04eb616f490e04cdc186f717
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-16 01:31:59 +0100

    Folder name change

--- Google Earth PHP KML/Further Development/displayWellregTestDB.php
+++ Google Earth PHP KML/Further Development/displayWellregTestDB.php
@@ -23,8 +23,9 @@
          include("dbconcomplex.php");
           //if(isset($_POST['wellregistrationtestdb'])) {
           $WellRegtest = $_POST['wellregistrationtestdb'];
-
-          $sql_query = "SELECT WellRegistration, ID, WellID, PercSand, Hydrocarbon, Cored FROM coredukwells where WellRegistration = '" . $WellReg ."'";
+          echo $WellReg . testtest;
+          //$sql_query = "SELECT WellRegistration, ID, WellID, PercSand, Hydrocarbon, Cored FROM coredukwells where WellRegistration = '" . $WellReg ."'";
+          $sql_query = "SELECT WellRegistration, ID, WellID, PercSand, Hydrocarbon, Cored FROM coredukwells where WellRegistration = '22/14b- 5'";
           //}
           //else {
           //$sql_query = "SELECT WellRegistration, ID, WellID, PercSand, Hydrocarbon, Cored FROM coredukwells";
