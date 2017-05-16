7db41363b327be04f1d41afaceb7cdee15e8c6da
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-16 01:45:26 +0100

    Folder name change

--- Google Earth PHP KML/Further Development/displayWellreg.php
+++ Google Earth PHP KML/Further Development/displayWellreg.php
@@ -26,7 +26,8 @@
              //if(isset($_POST['wellregistration'])) {
              $WellReg = $_POST["wellregistration"];
            echo $WellReg . testtest;
-             $sql_query = "SELECT RowID, WellRegistration, WellID, PercSand, Hydrocarbon, Cored, OrigID FROM coredUKHCorNotes where WellRegistration = '" . '$WellReg' . "'";
+             //$sql_query = "SELECT RowID, WellRegistration, WellID, PercSand, Hydrocarbon, Cored, OrigID FROM coredUKHCorNotes where WellRegistration = '" . '$WellReg' . "'";
+             $sql_query = "SELECT RowID, WellRegistration, WellID, PercSand, Hydrocarbon, Cored, OrigID FROM coredUKHCorNotes where WellRegistration = '" . ''$WellReg'' . "'";
              //$sql_query = "SELECT RowID, WellRegistration, WellID, PercSand, Hydrocarbon, Cored, OrigID FROM coredukhcornotes where WellRegistration = '''30/14- 3'''";
         //}
         //else {
