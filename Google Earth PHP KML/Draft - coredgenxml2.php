d1967178b982beffd69bc1937116fe8357e01528
Johnnie <j.morrison12@rgu.ac.uk>
2017-05-21 21:41:33 +0100

    Folder name change

--- Google Earth PHP KML/Draft - coredgenxml2.php
+++ Google Earth PHP KML/Draft - coredgenxml2.php
@@ -31,15 +31,15 @@ $opennode = $dom->createElement("open","1");
 $parnode->appendChild($opennode);
 
 // Opens a connection to a mySQL server
-$connection=mysql_connect($server, $username, $password);
+$connection=mysqli_connect($server, $username, $password);
 if (!$connection) {
-die("Not connected : " . mysql_error());
+die("Not connected : " . mysqli_error());
 }
 
 // Set the active mySQL database                            
-$db_selected = mysql_select_db($database, $connection);
+$db_selected = mysqli_select_db($database, $connection);
 if (!$db_selected) {
-die ("Can\'t use db : " . mysql_error());
+die ("Can\'t use db : " . mysqli_error());
 }
 
 // Search the rows in the coredukwells table
@@ -48,14 +48,14 @@ $query = "SELECT WellID, WellRegistration, LatDD, LonDD, ( 3959 * acos( cos("
 ."- radians(" . $center_lng . ") ) + sin( radians(".$center_lat.") ) *"
 ."sin( radians( LatDD ) ) ) ) AS distance FROM coredukwells HAVING distance < "
 .$radius. " ORDER BY distance LIMIT 0 , 20";
-$result = mysql_query($query);
+$result = mysqli_query($query);
 if (!$result) {
-die("Invalid query: " . mysql_error());
+die("Invalid query: " . mysqli_error());
 }
 
 //header("Content-type: application/vnd.google-earth.kml+xml");  
 // Iterate through the rows, adding XML nodes for each
-while ($row = @mysql_fetch_assoc($result)){
+while ($row = @mysqli_fetch_assoc($result)){
 $node = $dom->createElement("Placemark");
 $placenode = $parnode->appendChild($node);$namenode = $dom->createElement("name",htmlentities ($row['WellID']));
 $placenode->appendChild($namenode);
